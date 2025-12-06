9fa122ada8ac1d48c4f848180f5262f819b8ab0a
Ahmed Salem <superahmed208@gmail.com>
2025-12-06 14:46:25 -0500

    done search mvp

--- script.js
+++ script.js
@@ -18,9 +18,51 @@ function search(){
             searchContainer.innerHTML = 'No results found'
         }else{
             searchContainer.innerHTML = `Found ${data.count} results`
+            addSearchResults(data)
         }
     })
     .catch(error => {
         console.error('Error fetching data:', error)
+    })
+}
+
+function addSearchResults(data){
+    searchContainer = document.getElementById('searchContainer')
+    books = []
+    bookDivs = []
+
+    data.results.forEach(element => {
+        temp = {}
+        temp.id = element.id
+        temp.title = element.title
+        temp.authors = element.authors
+
+        books.push(temp)
+    })
+
+    books.forEach(book => {
+        newDiv = document.createElement('div')
+        newDiv.id = book.id
+        newDiv.class = 'searchResult'
+        newDiv.style.backgroundColor = 'lightgray'
+        newDiv.style.borderRadius = '5px'
+        newDiv.style.padding = '5px'
+        newDiv.style.margin = '10px'
+
+        newH2 = document.createElement('h2')
+        newH2.textContent = book.title
+        newH2.style.marginTop = '0.2rem'
+
+        newP = document.createElement('p')
+        newP.textContent = `Author: ${book.authors[0].name}`
+
+        newA = document.createElement('a')
+        newA.textContent = 'Read book'
+        newA.href = `/reader?book=${book.id}`
+
+        newDiv.append(newH2)
+        newDiv.append(newP)
+        newDiv.append(newA)
+        searchContainer.append(newDiv)
     })
 }
